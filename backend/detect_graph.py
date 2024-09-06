7bff9d77dab7bad035b15d838c3fb5c8db87f934
Julius Deynet <julius.deynet@stud-mail.uni-wuerzburg.de>
2024-09-06 11:20:43 +0200

    Add missing authors

--- backend/detect_graph.py
+++ backend/detect_graph.py
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2024 Julius Deynet <jdeynet@googlemail.com>
+# SPDX-FileCopyrightText: 2024 Tim Hegemann <tim.hegemann@uni-wuerzburg.de>
+# SPDX-FileCopyrightText: 2024 Sebastian Kempf <sebastian.kempf@uni-wuerzburg.de>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
